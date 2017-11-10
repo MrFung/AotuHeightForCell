cf6030b4811378cebe754472fc51b72310b97d5d
MrFung <2578033436@qq.com>
2017-11-10 17:30:24 +0800

    Initial Commit

---
+++ AotoHeightForCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AotoHeightForCell
+//
+//  Created by 郭枫 on 2017/11/10.
+//  Copyright © 2017年 郭枫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
